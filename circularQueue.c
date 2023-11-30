d481fb672fbc5083fa1a518e94cf566280ede377
Jibin Gigi <144718331+Jibin-Gigi@users.noreply.github.com>
2023-12-01 00:05:11 +0530

    Update circularQueue.c

--- circularQueue.c
+++ circularQueue.c
@@ -1,95 +1,106 @@
 #include<stdio.h>
-int front=-1,rear=-1,item,limit,queue[100];
-void Enqueue() {
-     if ((rear + 1) % limit == front) {
-        printf("Overflow\n");
-        }
-     else if (front == -1 && rear == -1) {
-        front = 0;
-        rear = 0;
-        printf("Enter the value to be stored: ");
-        scanf("%d", &item);
-        queue[rear] = item;
-    } 
-     
-    
-     else {
-        rear = (rear + 1) % limit;
-        printf("Enter the value to be stored: ");
-        scanf("%d", &item);
-        queue[rear] = item;
-    }
-}
-
-void Dequeue() {
-    if (front == -1 && rear == -1) {
-        printf("Underflow\n");
-    } else if (front == rear) {
-        item = queue[front];
-        printf("%d is removed",queue[front]);
-        front = -1;
-        rear = -1;
-    } else {
-        item = queue[front];
-        printf("%d is removed",queue[front]);
-        front = (front + 1) % limit;
-    }
-}
-
-void Peek(){
-      if(rear<front)
-         printf("The queue is empty\n");
-      else
-         {
-         printf("Peek value of queue=%d",queue[front]);
-         printf("\n");
-         }
-}
-void Display(){
-       if(front == -1 && rear == -1)
-         printf("The queue is empty\n");
-       else{
-         printf("The queue:");
-         for(int i=front;i!=rear;(i+1)%limit)
-         {
-         printf("%d\t",queue[i]);
-         }
-         printf("\n");
-         }
-         
-  
-}
-
-int main() {
-    int choice;
-    printf("Enter the Limit: ");
-    scanf("%d", &limit);
-
-    do {
-        printf("Enter the choice: \n1.Insertion\t2.Deletion\t3.Print peek element\t4.Display\t5.Exit\n");
-        scanf("%d", &choice);
-
-        switch (choice) {
-            case 1:
-                Enqueue();
-                break;
-            case 2:
-                Dequeue();
-                break;
-            case 3:
-                Peek();
-                break;
-            case 4:
-                Display();
-                break;
-            case 5:
-                break;
-            default:
-                printf("Invalid input\n");
-        }
-        
-    } while (choice != 5);
-
-    return 0;
-}
-
+int a[50],front=-1,rear=-1,max;
+void enqueue();
+void dequeue();
+void display();
+void main()
+ {
+   int choice;
+   printf("Enter the maximum of queue\n");
+   scanf("%d",&max);
+   do
+    {
+	printf("Enter the choice\t1)Enqueue\t2)Dequeue\t3)Display\t4)Exit\n");
+	scanf("%d",&choice);
+	switch(choice)
+	 {
+	  case 1:
+	   {
+	    enqueue();
+	    break;
+	   }
+	 case 2:
+	  {
+	   dequeue();
+	   break;
+	  }
+	case 3:
+	 {
+	  display();
+	  break;
+	 }
+	case 4:
+	 {
+	  break;
+	 }
+	default :
+	 {
+	  printf("Invalid choice\n");
+	 }
+	}
+     } while(choice!=4);
+ }
+void enqueue()
+ {
+   int val;
+   
+   if(front==(rear+1)%max)
+     {
+	printf("OVERFLOW\n");
+     }
+   else if(front==-1&&rear==-1)
+     {
+	printf("Enter the value to be added\n");
+   scanf("%d",&val);
+	front=0;
+	rear=0;
+	a[rear]=val;
+     }
+   
+   else
+     {
+       printf("Enter the value to be added\n");
+   scanf("%d",&val);
+       rear=(rear+1)%max;
+       a[rear]=val;
+     }
+ }
+void dequeue()
+ {
+  int item;
+  if(front==-1&&rear==-1)
+  {
+   printf("UNDERFLOW\n");
+  }
+ else if(front==rear)
+  {
+   item=a[front];
+   printf("The value %d is removed\n",item);
+   front=-1;
+   rear=-1;
+  }
+ else
+  {
+   item=a[front];
+   printf("The value %d is removed\n",item);
+   front=(front+1)%max;
+  }
+ }
+void display() 
+ {
+  int i;
+  if(front == -1)
+  printf("Empty Circular Queue, no elements to display\n");
+  else 
+   {
+    printf("\nElements of queue: ");
+    for (i = front; i != rear; i = (i + 1) %max) 
+     {
+     	printf("%d ", a[i]);
+     }
+     printf("%d ", a[i]);
+     printf("\nFront positions at %d \n", front);
+      printf("\nRear positions at %d \n", rear);
+   }
+  printf("\n");
+ }
